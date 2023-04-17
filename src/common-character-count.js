10cea7aa32ff646be0852960b1891a79de1e1330
dmitri4k <mdsunknown@gmail.com>
2023-04-18 02:13:43 +0300

    part 3

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -10,11 +10,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  aabcc aaacd
+  aacc  aaac
+*/
+function getCommonCharacterCount(s1, s2) {
+  const result = {}
+  const reference = [...s1]
+  let str = s2
+
+  for (const letter of reference) {
+    if (str.includes(letter)) {
+      if (!result[letter]) { result[letter] = 0; }
+      result[letter]++
+      str = str.replace(letter, "")
+    }
+  }
+
+  let output = []
+  output.push(`Strings have ${Object.values(result).reduce( (sum, val) => sum + val, 0)} common characters -`)
+  let first = true
+  
+  for (const prop in result) {
+    if (first) {first = false} 
+    else {output.push(` and`)}
+    output.push(` ${result[prop]} "${prop}"${(result[prop] > 1) ? 's' : ''}`)
+  }
+  output.push(`.`)
+  console.log(output.join``)
+
+  return Object.values(result).reduce( (sum, val) => sum + val, 0)
 }
+
+// Simple variant without text output;)
+/*
+function getCommonCharacterCount(s1, s2) {
+  const result = []
+  const reference = [...s1]
+  let str = s2
+
+  for (const letter of reference) {
+    if (str.includes(letter)) {
+      result.push(letter)
+      str = str.replace(letter, "")
+    }
+  }
+  return result.length
+} */
 
 module.exports = {
   getCommonCharacterCount
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+	let res = []
+  let end = arr.length
+  for (let i = 0; i < end ; i++) {
+	       if(arr[i] == '--double-next') {
+      if ( i != end-1 ) { res.push(arr[i+1]) }}
+
+		else if(arr[i] == '--discard-next') { i++}
+
+		else if(arr[i] == '--double-prev') {
+			if (i != 0 && arr[i-2] != '--discard-next') { res.push(arr[i-1]) }}
+
+		else if(arr[i] == '--discard-prev') { 
+      if (i != 0 && arr[i-2] != '--discard-next') { res.pop() }}
+
+    else { res.push(arr[i]) }
+	}
+  return res
 }
 
 module.exports = {
